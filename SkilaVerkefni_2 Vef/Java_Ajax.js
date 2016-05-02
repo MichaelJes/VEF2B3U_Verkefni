fb3237f90abf9e8fd56a137dbc5541d7ce359729
Michael Jes Þórsson <michaeltho236@nemi.tskoli.is>
2016-05-02 12:25:14 +0000

    Gleymdi smá

--- SkilaVerkefni_2 Vef/Java_Ajax.js
+++ SkilaVerkefni_2 Vef/Java_Ajax.js
@@ -5,8 +5,9 @@
     'success': function(response) {
         
         var data = 0;
-        $('main').append('' +'<div class=" pure-u-1 ">' + 
+        $('main').append('' +'<div id="search"class=" pure-u-1 ">' + 
             '<input type="text" placeholder="filter by search" id="filter-search">');
+        $('main').append('' +'<div id="Buttons" class=" pure-u-1 ">');
         for(var i = 0; i < response.results.length; i++){
             data = response.results;
             var date = moment(data[i].dateOfShow).format('DD.MMMM YYYY , h:mm');
@@ -24,31 +25,31 @@
                     $(this).unbind("error").attr("src", "Pictures/Kennari.jpg");
                 };
             });
-            $('main').append('' +'<div id="'+i+'" class=" pure-u-1 pure-u-md-1-3 pure-u-sm-1-2 pure-u-lg-1-4 pure-u-xl-1-5 Window">' +
+            $('main').append('' +'<div data-tags="'+data[i].eventHallName+'" id="'+i+'" class=" pure-u-1 pure-u-md-1-3 pure-u-sm-1-2 pure-u-lg-1-4 pure-u-xl-1-5 Window">' +
                         '<img  class="Humas" alt="'+data[i].eventDateName+'" src="' + data[i].imageSource + '">' +
                         '<div >'+
                             '<h2><b>' + data[i].eventDateName + '</b></h2>' +
                             '<h4>' + data[i].eventHallName + '</h4>' +
                             '<small>' +"Dagsetning "+ Dagsetning+" <br> "+"Klukkan "+ Timi + '</small>'+'</div>' +'</div>');
         }
-               (function() {                           // Lives in an IIFE
-              var $images = $('img');           // Get the images
-              var $search = $('#filter-search');      // Get the input element
-              var cache = [];                         // Create an array called cache
+               (function() {
+              var $images = $('img');           
+              var $search = $('#filter-search');      
+              var cache = [];                         
 
-                  $images.each(function() {                 // For each image
-                    cache.push({                          // Add an object to the cache array
-                      element: this,                      // This image
-                      text: this.alt.trim().toLowerCase() // Its alt text (lowercase trimmed)
+                  $images.each(function() {                 
+                    cache.push({                          
+                      element: this,                      
+                      text: this.alt.trim().toLowerCase() 
                     });
                   });
              
                 function filter() {   
-                        var idtimer = 0;                  // Declare filter() function
-                        var querys = this.value.trim().toLowerCase();  // Get the query
-                        cache.forEach(function(img) {         // For each entry in cache pass image 
-                          var index = 0;                      // Set index to 0
-                          if (querys) {                        // If there is some query text
+                        var idtimer = 0;                  
+                        var querys = this.value.trim().toLowerCase();  
+                        cache.forEach(function(img) {          
+                          var index = 0;                      
+                          if (querys) {                        
                             index = img.text.indexOf(querys);  // Find if query text is in there
                           }
                           if (index === -1) {$( ""+"#"+""+idtimer+"" ).hide()};
@@ -58,13 +59,58 @@
                         });
                       }
 
-                      if ('oninput' in $search[0]) {          // If browser supports input event
-                        $search.on('input', filter);          // Use input event to call filter()
-                      } else {                                // Otherwise
-                        $search.on('keyup', filter);          // Use keyup event to call filter()
+                      if ('oninput' in $search[0]) {          
+                        $search.on('input', filter);          
+                      } else {                                
+                        $search.on('keyup', filter);          
                       }              
 
                     }());
-       
-    }
+                        (function() {
+                        var $imgs = $('.Window');                  
+                        var $buttons = $('#Buttons');
+                        var tagged = {};
+                        $imgs.each(function() {                         
+                          var img = this;                               
+                          var tags = $(this).data('tags');                                             
+                        if (tags) {                                   
+                            tags.split(",").forEach(function(tagName) { 
+                              if (tagged[tagName] == null) {            
+                                tagged[tagName] = [];                   
+                              }
+                              tagged[tagName].push(img);                
+                            });
+                          }               
+                        });
+                            $('<button/>', {                                 
+                          text: 'Show All',                              
+                          class: 'active',
+                          click: function() {                            
+                            $(this)                                      
+                              .addClass('active')                        
+                              .siblings()                                
+                              .removeClass('active');                    
+                            $imgs.show();                                
+                          }
+                        }).appendTo($buttons);                           
+
+                        $.each(tagged, function(tagName) {               
+                          $('<button/>', {                               
+                            text: tagName + ' (' + tagged[tagName].length + ')', 
+                            click: function() {                          
+                              $(this)                                    
+                                .addClass('active')                      
+                                .siblings()                              
+                                .removeClass('active');                  
+                              $imgs                                      
+                                .hide()                                  
+                                .filter(tagged[tagName])                 
+                                .show();                                 
+                            }
+                          }).appendTo($buttons);                         
+                        });
+
+                      }());
+                                                   
+                          }
 })
